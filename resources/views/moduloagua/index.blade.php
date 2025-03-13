436bd0ddfaf2e22fb1302a74f4a76cfc073dd0de
mariagutierrez02 <mariagm.estadias@gmail.com>
2025-03-13 14:24:40 -0600

    prueba

--- resources/views/moduloagua/index.blade.php
+++ resources/views/moduloagua/index.blade.php
@@ -22,7 +22,7 @@
         
         <!-- Contenido del Módulo Agua -->
         <div class="w-full lg:w-2/3 p-8">
-            <h1 class="text-4xl font-bold text-blue-600 mb-8 text-center lg:text-left">Módulo Agua</h1>
+            <h1 class="text-4xl font-bold text-blue-600 mb-8 text-center lg:text-left">Módulo del Agua</h1>
             <p class="text-gray-600 text-lg mb-6">
                 Aquí podrás gestionar todo lo relacionado con el consumo y ahorro de agua dentro del Sistema QMS.
             </p>
